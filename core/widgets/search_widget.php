648a8f551d7eb1589e7b5efbe0382d786b33f163
qinshaoxuan <qinshaoxuan@baidu.com>
2016-11-30 23:32:21 +0800

    构造方法改用__construct

--- core/widgets/search_widget.php
+++ core/widgets/search_widget.php
@@ -1,50 +1,50 @@
-<?php 
+<?php
 
 /*-----------------------------------------------------------------------------------*/
 /* START SEARCH WIDGET
-/*-----------------------------------------------------------------------------------*/ 
-   
+/*-----------------------------------------------------------------------------------*/
+
 class suevaxuan_search_widget extends WP_Widget
 {
-	public function __construct() 
+	public function __construct()
     {
-		parent::WP_Widget( "suevaxuan_search_widget", "Wip Search Widget", array("description" => "Wip Search Widget"));
+		parent::__construct( "suevaxuan_search_widget", "Wip Search Widget", array("description" => "Wip Search Widget"));
 	}
 		public function form( $instance )
 	{
         /* Impostazioni di default del widget */
-		$defaults = array( 
+		$defaults = array(
 			'title' => 'Search Form',
         );
-		
-		$instance = wp_parse_args( (array) $instance, $defaults ); 
+
+		$instance = wp_parse_args( (array) $instance, $defaults );
 
 		?>
-        
+
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
 				Title:
 			</label>
 			<input class="widefat" type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
-			
-		</p>  
+
+		</p>
 
 		<?php
 	}
-	
-	public function update( $new_instance, $old_instance ) 
+
+	public function update( $new_instance, $old_instance )
     {
 		$instance = $old_instance;
 
 		$instance['title'] = strip_tags( $new_instance['title'] );
-		
+
 		return $instance;
-	}                     
+	}
 
 	public function widget( $args, $instance )
 	{
 		extract( $args );
-		
+
 		echo $before_widget;
 
 		$title = apply_filters( 'widget_title', $instance['title'] );
@@ -59,7 +59,7 @@ class suevaxuan_search_widget extends WP_Widget
     		     <input type="text" placeholder="<?php _e( 'Search here', 'wip' ) ?>"  name="s" id="s" class="input-search"/>
     			 </div>
 			</form>
-        <div class="clear"></div>  
+        <div class="clear"></div>
 		</section>
 		<?php
 		echo $after_widget;
@@ -73,5 +73,5 @@ add_action( 'widgets_init', 'add_suevaxuan_search_widget' );
 
 /*-----------------------------------------------------------------------------------*/
 /* END SEARCH WIDGET
-/*-----------------------------------------------------------------------------------*/    
+/*-----------------------------------------------------------------------------------*/
 ?>
